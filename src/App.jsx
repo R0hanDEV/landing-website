1d4bcffbd5b814482fa14e5702e5faf724caec7c
rohan <r0han45dev@gmail.com>
2024-08-10 13:29:01 +0530

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <>hello</>;
+  return (
+    <div className="min-height-rule">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
